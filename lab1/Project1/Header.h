b598fd731f6cfb517f559f505e537c7e5bf77bf8
aytsan-ns <149725956+aytsan-ns@users.noreply.github.com>
2024-03-17 21:42:51 +0300

    2

--- lab1/Project1/Header.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int CountNumbersWithSumK(int a, int b);
-int FindNumber(int n, int sum, int k, int count);
--- lab1/Project1/Source.c
+++
@@ -1,9 +0,0 @@
-#pragma warning (disable:4996)
-
-#include <stdio.h>
-#include "Header.h"
-
-int main() {
-	int n = 0, k = 1;
-	printf("%i", CountNumbersWithSumK(n, k));
-}
